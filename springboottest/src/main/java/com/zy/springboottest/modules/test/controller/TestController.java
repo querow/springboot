c2a1c7bc06f44c1fe0bc9a0070c88be57f59e6b7
querow <zhaoyi580@qq.com>
2020-08-10 19:43:19 +0800

    2020/08/10

---
+++ springboottest/src/main/java/com/zy/springboottest/modules/test/controller/TestController.java
@@ -0,0 +1,38 @@
+package com.zy.springboottest.modules.test.controller;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+@Controller
+@RequestMapping("/test")
+public class TestController {
+    private final static Logger LOGGER = LoggerFactory.getLogger(TestController.class);
+    @Value("${server.port}")
+    private int port;
+    @Value("${com.name}")
+    private String name;
+    @Value("${com.age}")
+    private int age;
+    @Value("${com.desc}")
+    private String desc;
+    @Value("${com.random}")
+    private String random;
+
+    @GetMapping("/logTest")
+    @ResponseBody
+    public String logTest(){
+        LOGGER.trace("This is trace log");
+        LOGGER.debug("This is debug log");
+        LOGGER.info("This is info log");
+        LOGGER.warn("This is warn log");
+        LOGGER.error("This is error log");
+        return "This is log test";
+    }
+}
