e09f03a36ec2189be060fb5e674022d0ff58904a
querow <zhaoyi580@qq.com>
2020-09-08 15:37:40 +0800

    清理一下

--- springboottest/src/main/java/com/zy/springboottest/modules/test/service/CardService.java
+++
@@ -1,8 +0,0 @@
-package com.zy.springboottest.modules.test.service;
-
-import com.zy.springboottest.modules.common.vo.Result;
-import com.zy.springboottest.modules.test.entity.Card;
-
-public interface CardService {
-    Result<Card> insertCard(Card card);
-}
